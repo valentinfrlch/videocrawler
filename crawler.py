5b04ccbb9d0283efcd53628b16b2c3bb5801af12
valentinfrlch <valentin.froehlich@gmail.com>
2022-09-29 19:13:27 +0200

    switched to requests_html

--- crawler.py
+++ crawler.py
@@ -1,33 +1,15 @@
 # take a url as argument
-
-import kwargs
-import requests
-from bs4 import BeautifulSoup
+# crawl a page with javascript
+import requests_html
 
 
-def crawler(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # find all the links in the page
-    links = soup.find_all('a')
-    output = []
-    for link in links:
-        try:
-            # crawl the link
-            # get the html
-            r = requests.get(link.get('href'))
-            # parse with BeautifulSoup
-            soup = BeautifulSoup(r.text, 'html.parser')
-            # find all video links
-            videos = soup.find_all('video')
-            # add the video links to the output
-            for video in videos:
-                print(url + video.get('src'))
-                output.append(url + video.get('src'))
-
-        except Exception as e:
-            continue
-    return output
-
-
-print(crawler('https://www.ardmediathek.de'))
+def crawler(video):
+    # make a request then render the page
+    url = "https://hdtoday.cc/search/" + video
+    session = requests_html.HTMLSession()
+    r = session.get(url)
+    # render the page
+    html = r.html.render()
+    return html
+    
+print(crawler("Bullet Train"))
